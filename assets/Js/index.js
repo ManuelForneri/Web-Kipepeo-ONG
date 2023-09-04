e8f0eba2c50268656ab9326171d62b0c23091858
ManuelForneri <manuelforneri@hotmail.com>
2023-09-04 18:57:36 -0300

    add toggleOptionsButtons

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -4,3 +4,12 @@ const navLinks = document.querySelector(".nav-links");
 burger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+function toggleOptions(optionsId) {
+  var options = document.getElementById(optionsId);
+  if (options.style.display === "block") {
+    options.style.display = "none";
+  } else {
+    options.style.display = "block";
+  }
+}
